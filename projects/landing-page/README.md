f414dc21f0fb936c509867a729d5ec14c8d2f85f
RehamAbdulhameed <53119319+RehamAbdulhameed@users.noreply.github.com>
2021-05-11 21:35:28 +0200

    Solution V0.1
    solution to the landing page project with  dynamically built navigation with scroll on click function and section active state to the current viewed section.

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -2,12 +2,14 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+* [Solution](#solution)
 
-## Instructions
+## Solution
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+The final project has some HTML and CSS styling and JavaScript to display an interactive version of the Landing Page project. 
 
-To get started, open `js/app.js` and start building out the app's functionality
+This Landing page have the following behavior : 
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+- An unordered list that forms navigation. which built dynamically.
+- Section Active state to show which section is being viewed while scrolling through the page.
+- Scroll to the appropriate section when clicking an item from the navigation menu. 
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,15 +17,15 @@
  * Define Global Variables
  * 
 */
-
-
+const navList = document.querySelector('#navbar__list')
+const sections = document.querySelectorAll('section');
+const fragment = document.createDocumentFragment();
+let items; 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
 
 /**
  * End Helper Functions
@@ -35,8 +35,43 @@
 
 // build the nav
 
+function createNavList() {
+
+    sections.forEach((sec) => {
+        const listItem = document.createElement('li');
+        listItem.innerText = sec.firstElementChild.firstElementChild.textContent;
+        listItem.classList.add('menu__link');
+
+
+        // Scroll to anchor ID using scrollTO event
+        listItem.addEventListener('click', () => {
+            window.scrollTo({ left: sec.offsetLeft, top: sec.offsetTop, behavior: 'smooth' })
+        });
+
+        fragment.appendChild(listItem);
+    })  
+}
+
+
 
 // Add class 'active' to section when near top of viewport
+
+function checkActive() {
+    items = document.querySelectorAll('li');
+    const trigger = window.innerHeight / 5 ;
+    
+    items.forEach((item, idx) => {
+        
+        const sectionTop = sections[idx].getBoundingClientRect().top
+        const sectionBottom = sections[idx].getBoundingClientRect().bottom
+
+        if (sectionTop < trigger && sectionBottom> trigger) {
+            item.classList.add('active')
+        } else {
+            item.classList.remove('active')
+        }
+    })
+}
 
 
 // Scroll to anchor ID using scrollTO event
@@ -50,8 +85,12 @@
 
 // Build menu 
 
+createNavList();
+navList.appendChild(fragment);
+
 // Scroll to section on link click
+//within createNavList()
 
 // Set sections as active
 
-
+window.addEventListener('scroll', checkActive);
